aca629e9f9f61c8261f01ed2202e8eacd596909c
jawad571 <jawadmustafa571@gmail.com>
2023-11-28 23:41:15 +0000

    upload project

---
+++ resume-data.js
@@ -0,0 +1,86 @@
+const name = "Jawad Mustafa"
+const title = "Software Engineer"
+const email = "jawadmustafa571@gmail.com"
+const phone = "+(44) 7774271237"
+const linkedin = "https://www.linkedin.com/in/jawad57/"
+const github = "https://github.com/jawad571"
+
+const resumeData = {
+    "education": [
+      {
+        "degree": "MSc, Advanced Computer Science.",
+        "university": "UNIVERSITY OF LEICESTER",
+        "graduationYear": "September 2024",
+        "description": [`Relevant Coursework: Advanced Web Technologies, Agile Cloud Automation, Internet and Cloud
+        Computing, Big Data and Predictive Analytics, Foundations of Cybersecurity, Generative Development,
+        Service-Oriented Architectures.`]
+      },
+      {
+        "degree": "BSc, Computer Science",
+        "university": "IBA-KARACHI",
+        "graduationYear": "August 2022",
+        "description": [
+          "Completed General Electives: Introduction to Comparative Politics, Introduction to Differential Equations, Optimization Techniques.",
+          "Completed Domain Electives: Introduction to Robotics, Network Security, Cloud Computing, System Modeling and Simulation, Introduction to Data Mining, Enterprise Resource Planning.",
+          "Diverse elective courses helped broaden my knowledge and skills."
+        ]
+      }
+    ],
+    "experience": [
+      {
+        "title": "Software Engineer II",
+        "company": "10 PEARLS",
+        "startDate": "June 2023",
+        "endDate": "September 2023",
+        "responsibilities": [
+          "Reviewed Pull Requests daily to ensure timely delivery of the bi-weekly release.",
+          "Responsible for deploying code on the End to End (E2E) environment at the end of every release for functional testing.",
+          "Create or edit APIs as per the need of the task at hand.",
+          "Pick up at least 2 high-priority defects and tickets related to new features to meet the client’s needs in every release."
+        ]
+      },
+      {
+        "title": "Associate Software Engineer",
+        "company": "10 PEARLS",
+        "startDate": "June 2022",
+        "endDate": "June 2023",
+        "responsibilities": [
+          "Enhanced code efficiency by refactoring and optimizing data pre-processing operations.",
+          "Played a key role in the development, maintenance, and enhancement of the ML pipeline, while automating the Model training process to achieve a remarkable reduction of 2 hours in training time.",
+          "Created a comprehensive back-testing framework from the ground up for the recommendation engine, enabling in-depth comparative analysis following each version update."
+        ]
+      },
+      {
+        "title": "Research Assistant",
+        "company": "IBA KARACHI",
+        "startDate": "October 2021",
+        "endDate": "January 2022",
+        "responsibilities": [
+          "Revamped and expanded the capabilities of the chatbot at Devaj, resulting in improved functionality and enhanced user experience.",
+          "Led the successful migration of the bot from RASA to core Python, optimizing server resources."
+        ]
+      },
+      {
+        "title": "Software Intern",
+        "company": "DEVAJ TECHNOLOGY",
+        "startDate": "June 2021",
+        "endDate": "October 2021",
+        "responsibilities": [
+          "Conducted a comparative analysis of 2 chatbot frameworks | RASA | Dialogue Flow.",
+          "Implemented voice recognition functionality into the chatbot, enabling it to effectively respond to user voicemails.",
+          "Migrated the MVP from in-house server to AWS EC2 to ensure high availability.",
+          "The newly developed chatbot could potentially increase sales due to easy accessibility for customers."
+        ]
+      },
+    ],
+    "projects": [
+      {
+        "title": "Project 1",
+        "description": "Description of Project 1."
+      },
+      {
+        "title": "Project 2",
+        "description": "Description of Project 2."
+      }
+    ]
+  }
